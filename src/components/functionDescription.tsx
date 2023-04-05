0e3f75e4f4ebf78e4462ad6347df4be2e48fe759
malaika.mbang@qmbase.com <malaika.mbang@qmbase.com>
2023-04-05 14:36:19 +0200

    Shared Description

--- src/components/functionDescription.tsx
+++ src/components/functionDescription.tsx
@@ -10,31 +10,31 @@ export const dataSharedFunction: SharedFunctionItem[] = [
   {
     id: SharedFunction.AuditTrail,
     title: SharedFunction.AuditTrail.toString(),
-    description: "Das ist die Beschreibung",
+    description: "Durch den Audit Trail können vorgenommene Änderungen innerhalb von qmbase verfolgt werden.",
   },
   {
     id: SharedFunction.Comments,
     title: SharedFunction.Comments.toString(),
-    description: "",
+    description: "Kommentare erleichtern die Zusammenarbeit und ermöglichen Nutzer ohne Zugriffsrechte auf Einträge, Informationen hinzuzufügen und zu ergänzen.",
   },
   {
     id: SharedFunction.Costs,
     title: SharedFunction.Costs.toString(),
-    description: "",
+    description: "Hier werden die Kosten für jeden Eintrag erfasst und anhand verschiedener Kriterien aufgeteilt.",
   },
   {
     id: SharedFunction.CustomFields,
     title: SharedFunction.CustomFields.toString(),
-    description: "",
+    description: "Vorhandene Formulare können vollständig an individuelle Bedürfnisse angepasst werden.",
   },
   {
     id: SharedFunction.Files,
     title: SharedFunction.Files.toString(),
-    description: "",
+    description: "Nutzen Sie Anhänge, um ergänzende Informationen aus anderen Quellen hinzuzufügen.",
   },
   {
     id: SharedFunction.Workflows,
     title: SharedFunction.Workflows.toString(),
-    description: "",
+    description: "Individuell festgelegte Workflows bieten die Möglichkeit, Prozesse zu automatisieren und präzise zu steuern.",
   },
 ];
